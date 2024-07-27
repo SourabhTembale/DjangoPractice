5d014d70426ebfc2fd43cb51adc587dfff05744f
SourabhTembale <107642036+SourabhTembale@users.noreply.github.com>
2024-07-27 15:01:38 +0530

    Update models.py

--- movies/models.py
+++ movies/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Movie(models.Model):
-    Series_Title = models.CharField(max_length=225)
-    Released_year = models.IntegerField()
-    Genre = models.CharField(max_length=225)
-    IMDB_rating = models.FloatField()
-
-    def __str__(self):
-        return self.title
